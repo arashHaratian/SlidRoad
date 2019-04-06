a74b825bf4be5a148bc8490bae535b9032e14598
Armin Nazari <anphoenix1999@gmail.com>
2019-04-06 11:48:10 +0430

    player and camera are be matched

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+    public GameObject playerDirection;
+
+    private Rigidbody playerRigidbody;
+    private Rigidbody playerDirectionRigidbody;    
+ 
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerDirectionRigidbody = playerDirection.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        playerRigidbody.velocity = moveSpeed * playerDirection.transform.forward;
+        playerDirectionRigidbody.velocity = moveSpeed * playerDirection.transform.forward;
+        
+        float axisRotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        playerDirectionRigidbody.angularVelocity = new Vector3(0, axisRotation, 0);
+    }
+}
