89cd8ff44f203c8eb9933ce5d60a1edabda0e9c3
Armin Nazari <anphoenix1999@gmail.com>
2019-04-07 14:26:48 +0430

    move and rotate road

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotateSpeed;
+    // Update is called once per frame\
+    public void Rotate(float rotate, Transform center)
+    {
+        transform.RotateAround(center.position, Vector3.up, rotateSpeed * rotate);
+    }
+}
