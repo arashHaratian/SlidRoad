827173687201b9f5df055518e970d753b154d95c
Armin Nazari <anphoenix1999@gmail.com>
2019-04-09 18:40:18 +0430

    refactoring start menu panel

--- Assets/Scripts/Buttuns.cs
+++ Assets/Scripts/Buttuns.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Buttuns : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ClickStart()
     {
         Debug.Log("start clicked");
