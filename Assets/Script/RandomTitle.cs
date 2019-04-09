50a0f07cecde73fc943dae47e211374cdaac3792
Mahsa Ghalevand <mahsa081997@gmail.com>
2019-04-09 01:25:51 -0700

    create simple road

---
+++ Assets/Script/RandomTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class RandomTitle : MonoBehaviour
+{
+    public GameObject title1;
+    public GameObject title2;
+    private int Num;
+
+    void Start()
+    {
+        Num = Random.Range(1, 2);
+        
+        if (Num == 1)
+        {
+            Instantiate(title1, transform.position, transform.rotation);
+        }
+
+        else if (Num == 2)
+        {
+            Instantiate(title2, transform.position, transform.rotation);
+        }
+    }
+  
+}
---
+++ Assets/Script/RoadGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour
+{
+public GameObject[] tilePrefabs;
+    
+private Transform playerTransform;
+private float spawnZ = -6.0f;
+private float tileLength = 12.0f;
+private int amountTitlesOnScreen = 6;
+private float safeZone = 15.0f;
+private int lastPrefabIndex = 0;
+
+private List<GameObject> activeTiles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       activeTiles = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < amountTitlesOnScreen; i++)
+        {
+            if (i < 2)
+            SpawnTile(0);
+            else
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (playerTransform.position.z - safeZone > (spawnZ - amountTitlesOnScreen * tileLength))
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        if(prefabIndex == -1)
+        go = Instantiate(tilePrefabs[RandomPrefabIndex()]) as GameObject;
+        else
+        go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+        activeTiles.Add(go);
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (tilePrefabs.Length <= 1)
+            return 0;
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, tilePrefabs.Length);
+        }
+
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+    }
+
+
+    
+}
