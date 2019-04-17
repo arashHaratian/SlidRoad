7adc4c1b630d5a11a94db77bf9e7a3ae70f49cca
Armin Nazari <anphoenix1999@gmail.com>
2019-04-17 14:06:34 +0430

    add limited speed rotation

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,21 @@ public class Rotation : MonoBehaviour
 {
     // Start is called before the first frame update
     public float rotateSpeed;
+
+    public float limitSpeed;
+
+    private float finalSpeed; 
     // Update is called once per frame\
+
     public void Rotate(float rotate, Transform center)
     {
-        transform.RotateAround(center.position, Vector3.up, rotateSpeed * rotate);
+        finalSpeed = rotate * rotateSpeed;
+        if(Math.Abs(finalSpeed) > limitSpeed)
+            if (finalSpeed > 0)
+                finalSpeed = limitSpeed;
+            else
+                finalSpeed = -1 * limitSpeed;
+        transform.RotateAround(center.position, Vector3.up, finalSpeed);
+        print(Math.Abs(rotate * rotateSpeed));
     }
 }
