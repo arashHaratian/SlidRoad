7ec2060d38798803e7ed01d724e94560789e87b2
Armin Nazari <anphoenix1999@gmail.com>
2019-04-20 12:18:51 +0430

    add GameOver script

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int gameOverHeigh = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerManager.instance.transform.position.y < gameOverHeigh)
+            GameManager.instance.GameOver = true;
+    }
+}
