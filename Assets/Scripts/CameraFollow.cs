389e0e8c3640268f8c6c6111114bbee8e8b24450
Arash <arashharatian@hotmail.com>
2019-04-21 13:34:49 +0430

    adding camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smoothing = 5f;
+    public Transform target;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+        offset.y += 2;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
