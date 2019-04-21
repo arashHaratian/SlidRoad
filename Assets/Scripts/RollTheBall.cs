93bae76ce5bfc6b020bdd7c335bdc6a219c05946
ArashHaratian <arashharatian@hotmail.com>
2019-04-21 14:51:33 +0430

    first rotate script

---
+++ Assets/Scripts/RollTheBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollTheBall : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(-RoadGenerator.Instance.ActiveTiles[0].transform.position.z / 5, 0, -RoadGenerator.Instance.ActiveTiles[0].transform.position.x / 3);
+    }
+}
