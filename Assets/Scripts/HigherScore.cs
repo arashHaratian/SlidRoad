086ba5d62471d552b9101ef79b2b6d4412d2382a
ArashHaratian <arashharatian@hotmail.com>
2019-04-23 00:56:08 +0430

    not completed commiting

---
+++ Assets/Scripts/HigherScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HigherScore : MonoBehaviour
+{
+    IEnumerator scoreBounce()
+    {
+        ScoreManager.score += 100f;
+        yield return new WaitForSeconds(3);
+        
+    }
+
+    // Start is called before the first frame update
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.CompareTag("Player"))
+            {
+                StartCoroutine("scoreBounce");
+                print("a");
+                break;
+            }
+        }
+        StopCoroutine("scoreBounce");
+    }
+}
