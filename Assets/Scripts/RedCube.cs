6ebffef6f7d2eb3b35d95bd973654b4a9ba51cdd
Armin Nazari <anphoenix1999@gmail.com>
2019-04-30 11:56:49 +0430

    ontriger and rotate script

---
+++ Assets/Scripts/RedCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedCube : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(Vector3.one * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.combo = 1;
+            Destroy(this.gameObject);
+            ShowCombo.Instance.UpdateText("");
+        }
+    }
+}
