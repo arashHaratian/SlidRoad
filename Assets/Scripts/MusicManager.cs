9cc96dc78187841eabd31769784380a21b7acef3
ArashHaratian <arashharatian@hotmail.com>
2019-05-01 18:31:56 +0430

    first commit for music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance = null;
+
+    public AudioMixer musicMixer;
+
+    AudioSource musicSource;
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+    }
+
+    public IEnumerator gameOVerEffect()
+    {
+        while (musicSource.pitch > 0 && GameManager.instance.GameOver)
+        {
+            musicSource.pitch -= 0.07f;
+            if (musicSource.pitch < 0)
+                musicSource.pitch = 0;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+    }
+
+    public void StartMusic()
+    { 
+        musicSource.enabled = false;
+        musicSource.pitch = 1;
+        musicSource.enabled = true;
+        musicSource.Play();
+    }
+    public void increaseMusicSpeed(float speed)
+    {
+        musicSource.pitch *= speed;
+        if (musicSource.pitch > 3)
+            musicSource.pitch = 3;
+    }
+}
