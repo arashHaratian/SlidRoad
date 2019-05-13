c790373d9b56848581ba480d6dcdefc4a1d69b68
ArashHaratian <arashharatian@hotmail.com>
2019-05-13 21:37:02 +0430

    eating Red Boxnow have effect

---
+++ Assets/Scripts/EatingBoxes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EatingBoxes : MonoBehaviour
+{
+    private Image eatingBoxImage;
+    private bool lastIsRedEated;
+
+    public Color flashColour; /*= new Color(1, 0, 0, 200);*/
+    public float fadingTime;
+
+    private void Awake()
+    {
+        eatingBoxImage = GetComponent<Image>();
+        lastIsRedEated = false;
+    }
+
+    private void Update()
+    {
+        lastIsRedEated = RedCube.isRedEated;
+        if (lastIsRedEated)
+            eatingBoxImage.color = flashColour;
+        else
+            eatingBoxImage.color = Color.Lerp(eatingBoxImage.color, Color.clear, fadingTime * Time.deltaTime);
+        RedCube.isRedEated = false;
+    }
+}
