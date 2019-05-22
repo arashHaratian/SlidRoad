8528e12a060db46bd953bddd50d7ff5e0dae4ce6
ArashHaratian <arashharatian@hotmail.com>
2019-05-22 13:45:12 +0430

    adding GA obj and GA cs

---
+++ Assets/Scripts/GameAnalyticsEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+public class GameAnalyticsEvent : MonoBehaviour
+{
+    public static GameAnalyticsEvent Instance;
+    private void Awake()
+    {
+        if (!Instance)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(this.gameObject);
+        GameAnalytics.Initialize();
+    }
+}
