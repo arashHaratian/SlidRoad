8d0e5d3e5f83d6c7a4985acc8dca9d724d09e51b
Armin Nazari <anphoenix1999@gmail.com>
2019-05-26 18:08:18 +0430

    cant fix gameOver just from forward

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float sideCubeSize;
+    public float ballRadius;
+    private Vector3 playerPosition;
+    private Vector3 cubePosition;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            GameOverManager.instance.Collision();
+            playerPosition = PlayerManager.instance.transform.position;
+            cubePosition = transform.position;
+//            if (playerPosition.x < cubePosition.x + (sideCubeSize / 2) &&
+//                playerPosition.x > cubePosition.x - (sideCubeSize / 2))
+//                ForwardCollision();
+            if (playerPosition.x > cubePosition.x)
+                RightCollision();
+            else if (playerPosition.x < cubePosition.x)
+                LeftCollision();
         }
+    }
+    private void RightCollision()
+    {
+        print("write");
+        playerPosition.x = cubePosition.x + sideCubeSize / 2 + ballRadius;
+        PlayerManager.instance.transform.position = playerPosition;
+    }
+
+    private void LeftCollision()
+    {
+        print("left");
+        playerPosition.x = cubePosition.x - sideCubeSize / 2 - ballRadius;
+        PlayerManager.instance.transform.position = playerPosition;
+    }
+    private void ForwardCollision()
+    {
+        print("forward");
+//        GameOverManager.instance.Collision();
     }
     
 }
