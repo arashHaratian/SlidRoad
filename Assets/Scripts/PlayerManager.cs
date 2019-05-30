6aef507f5064f2350c7fa1b3274edbd6aec694df
Armin Nazari <anphoenix1999@gmail.com>
2019-05-30 15:11:21 +0430

    balance sounds volume

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -119,7 +119,7 @@ public class PlayerManager : MonoBehaviour
             LeftMove(newPlayerPosition);
         else
             RightMove(newPlayerPosition);
-        SoundManager.instance.ChangeVolumeMovement(Math.Abs(movementValue));
+        SoundManager.instance.ChangeVolumeMovement(Math.Abs(movementValue) * 3);
     }
 
     void RightMove(Vector3 newPlayerPosition)
