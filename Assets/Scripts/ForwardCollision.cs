c7b005615db0fdcd852244f4273ebd2fa2594a94
Armin Nazari <anphoenix1999@gmail.com>
2019-05-30 20:07:19 +0430

    palyer move back during collision

---
+++ Assets/Scripts/ForwardCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardCollision : MonoBehaviour
+{
+    public LayerMask block;
+    public float radius;
+
+    private Vector3 endPoint;
+    private RaycastHit hit;
+    private Vector3 newPlayerPosition;
+    
+    void Update()
+    {
+        endPoint = transform.position;
+        endPoint.z += radius + 0.05f;
+        if (!IsCollision())
+        {
+            newPlayerPosition = hit.point;
+            newPlayerPosition.z -= radius;
+            transform.position = newPlayerPosition;
+        }
+    }
+
+    private bool IsCollision()
+    {
+           return !Physics.Linecast(transform.position, endPoint, out hit, block);
+    }
+}
