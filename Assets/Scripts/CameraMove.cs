2e647101cbbc2951dde4d46e893c3c58ee367a16
ArashHaratian <arashharatian@hotmail.com>
2019-05-31 22:00:00 +0430

    changing cameramera move

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Vector3 defualtPosition;
+
+    public float smoothing = 5f;
+    public Transform target;
+
+    Vector3 offset;
+
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = defualtPosition;
+        offset = transform.position - target.position;
     }
 
     public void Reset()
@@ -20,5 +28,12 @@ public class CameraMove : MonoBehaviour
     public void Move(float Movement)
     {
         transform.position += Vector3.right * Movement;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        targetCamPos.x = transform.position.x;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
 }
