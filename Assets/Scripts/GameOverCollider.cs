51e64b700a1b19a47cd2aab95a0507d62172c0ee
ArashHaratian <arashharatian@hotmail.com>
2019-05-31 17:42:10 +0430

    testing new gammeover

---
+++ Assets/Scripts/GameOverCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverCollider : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Update()
+    {
+        if(player.transform.position.z < 5)
+        {
+            player.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if (player.transform.position.z > 5f)
+            player.transform.position -= Vector3.forward * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            GameManager.instance.GameOver = true;
+    }
+}
