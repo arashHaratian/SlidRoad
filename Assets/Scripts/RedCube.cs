4d83fb6d4e76dc37d234e519c7fdde247483b17c
ArashHaratian <arashharatian@hotmail.com>
2019-06-08 14:44:54 +0430

    reset particles in red cube

--- Assets/Scripts/RedCube.cs
+++ Assets/Scripts/RedCube.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class RedCube : MonoBehaviour
 {
-    public float speed;
     public static bool isRedEated = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            PlayerManager.instance.tailParticles.SetActive(true);
+            PlayerManager.instance.fireParticles.SetActive(false);
+            PlayerManager.instance.smokeParticles.SetActive(false);
             ColorManager.instance.firstStage();
             isRedEated = true;
             ScoreManager.combo = 1;
