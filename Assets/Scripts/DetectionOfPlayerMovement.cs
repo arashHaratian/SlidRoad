8a22b55aa394ace08cd27952843ba36e266ad3cd
ArashHaratian <arashharatian@hotmail.com>
2019-06-08 22:59:40 +0430

    adding game meover NOT TESTED

--- Assets/Scripts/DetectionOfPlayerMovement.cs
+++ Assets/Scripts/DetectionOfPlayerMovement.cs
@@ -6,51 +6,35 @@ using UnityEngine.Serialization;
 
 public class DetectionOfPlayerMovement : MonoBehaviour
 {  
-    public static int num ;
+    public static int num;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (RoadGenerator.Instance.startGame)
-            {
-                if (RoadGenerator.Instance.counts <= 8)
-                {
-                    if (RoadGenerator.Instance.counts <= 4)
-                        RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.easyTilePrefabs);
-                    else
-                        RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.mediumTilePrefabs);
-                }
-                else
-                {
+            if (RoadGenerator.Instance.counts - 1 < RoadGenerator.Instance.easyTilePrefabs.Length)
+                RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.easyTilePrefabs);
 
-                    if (RoadGenerator.Instance.counts <= 30)
-                    {
-
-                        if (num <= 1)
-                            RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.hardTilePrefabs);
-
-                        else if (num == 2)
-                            RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.easyTilePrefabs);
-
-                        else if (num == 3 || num == 4)
-                            RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.mediumTilePrefabs);
-
-                        num++;
-
-                        if (num == 5)
-                            num = 0;
-                    }
-
-                    else
-                        RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.withoutTheWallTilePrefabs);
-                }
-                PackageManager.Instance.InsertPackage(RoadGenerator.Instance.ActiveTiles[3]);
-            }
             else
-                RoadGenerator.Instance.SpawnSmoothTile();
-            
+            {
+                if (num == 0)
+                    RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.easyTilePrefabs);
+
+                else if (num == 1 || num == 2)
+                    RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.mediumTilePrefabs);
+
+                else if (num >= 3)
+                    RoadGenerator.Instance.SpawnTile(RoadGenerator.Instance.hardTilePrefabs);
+
+                num++;
+
+                if (num == 6)
+                    num = 0;
+            }
+
             if (RoadGenerator.Instance.ActiveTiles.Count > RoadGenerator.Instance.roadOnScreen)
                 DeleteRoad();
+            GameOverManager.instance.CalGameOverPosition(RoadGenerator.Instance.CurrentRoad);
+            PackageManager.Instance.InsertPackage(RoadGenerator.Instance.ActiveTiles[3]);
         }
     }
 
@@ -58,5 +42,4 @@ public class DetectionOfPlayerMovement : MonoBehaviour
     {
         RoadGenerator.Instance.DeleteTile();
     }
-    
 }
