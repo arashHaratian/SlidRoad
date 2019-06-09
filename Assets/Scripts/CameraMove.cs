4d9e965b18bf6c7af47f4f3679d58874eaa689b7
ArashHaratian <arashharatian@hotmail.com>
2019-06-09 18:14:17 +0430

    addig camera move

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 defualtPosition;
+
+    public float smoothing = 5f;
+    public Transform target;
+
+    Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = defualtPosition;
+        offset = transform.position - target.position;
+    }
+
+    //public void Reset()
+    //{
+    //    transform.position = defualtPosition;
+    //}
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        targetCamPos.x = transform.position.x;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
