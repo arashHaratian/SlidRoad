a4be37949c5e4db327361fe0a1134af514357805
Mahsa <mahsa081997@gmail.com>
2019-08-20 11:43:41 +0400

    use test ads

--- Assets/Scripts/GoogleMobileAdsBanner.cs
+++ Assets/Scripts/GoogleMobileAdsBanner.cs
@@ -26,28 +26,28 @@ public class GoogleMobileAdsBanner : MonoBehaviour
 
     private void RequestBanner()
     {
-#if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-1278119892895852/2602063750";
-#elif UNITY_IPHONE
-            string adUnitId = "ca-app-pub-1278119892895852/5564103167";
-#else
-            string adUnitId = "unexpected_platform";
-#endif
+ #if UNITY_ANDROID
+      string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+  #elif UNITY_IPHONE
+      string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+  #else
+      string adUnitId = "unexpected_platform";
+  #endif
 
         // Create a 320x50 banner at the top of the screen.
         bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
         
-        // Called when an ad request has successfully loaded.
-        bannerView.OnAdLoaded += HandleOnAdLoaded;
-        // Called when an ad request failed to load.
-        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
-        // Called when an ad is clicked.
-        bannerView.OnAdOpening += HandleOnAdOpened;
-        // Called when the user returned from the app after an ad click.
-        bannerView.OnAdClosed += HandleOnAdClosed;
-        // Called when the ad click caused the user to leave the application.
-        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
-            
+//        // Called when an ad request has successfully loaded.
+//        bannerView.OnAdLoaded += HandleOnAdLoaded;
+//        // Called when an ad request failed to load.
+//        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+//        // Called when an ad is clicked.
+//        bannerView.OnAdOpening += HandleOnAdOpened;
+//        // Called when the user returned from the app after an ad click.
+//        bannerView.OnAdClosed += HandleOnAdClosed;
+//        // Called when the ad click caused the user to leave the application.
+//        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+//            
 
         
         // Create an empty ad request.
@@ -61,31 +61,31 @@ public class GoogleMobileAdsBanner : MonoBehaviour
 
     
     
-    public void HandleOnAdLoaded(object sender, System.EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdLoaded event received");
-    }
-
-    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    {
-        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
-                            + args.Message);
-    }
-
-    public void HandleOnAdOpened(object sender, System.EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdOpened event received");
-    }
-
-    public void HandleOnAdClosed(object sender, System.EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdClosed event received");
-    }
-
-    public void HandleOnAdLeavingApplication(object sender, System.EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdLeavingApplication event received");
-    }
+//    public void HandleOnAdLoaded(object sender, System.EventArgs args)
+//    {
+//        MonoBehaviour.print("HandleAdLoaded event received");
+//    }
+//
+//    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+//    {
+//        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+//                            + args.Message);
+//    }
+//
+//    public void HandleOnAdOpened(object sender, System.EventArgs args)
+//    {
+//        MonoBehaviour.print("HandleAdOpened event received");
+//    }
+//
+//    public void HandleOnAdClosed(object sender, System.EventArgs args)
+//    {
+//        MonoBehaviour.print("HandleAdClosed event received");
+//    }
+//
+//    public void HandleOnAdLeavingApplication(object sender, System.EventArgs args)
+//    {
+//        MonoBehaviour.print("HandleAdLeavingApplication event received");
+//    }
 
 
 }
