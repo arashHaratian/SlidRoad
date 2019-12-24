c4f00ade333db2bc3ad0e0c96ca38ff1af75aace
Mahsa <mahsa081997@gmail.com>
2019-12-24 16:47:41 +0330

    add particle for end line

---
+++ Assets/Scripts/PlayParticleInEndGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleInEndGame : MonoBehaviour
+{
+    
+    public ParticleSystem effect1;
+    public ParticleSystem effect2;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(effect1);
+            effect1.Play();
+            
+            Instantiate(effect2);
+            effect2.Play();
+        }
+    }
+}
